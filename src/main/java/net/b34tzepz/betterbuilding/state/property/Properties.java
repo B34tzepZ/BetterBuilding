aa9286d23ba9b83761288abd0c8a8fb1ccced7eb
B34tzepZ <florian.graeber@gmx.de>
2022-05-13 11:31:54 +0200

    corner shape property

--- src/main/java/net/b34tzepz/betterbuilding/state/property/Properties.java
+++ src/main/java/net/b34tzepz/betterbuilding/state/property/Properties.java
@@ -1,8 +1,10 @@
 package net.b34tzepz.betterbuilding.state.property;
 
+import net.b34tzepz.betterbuilding.block.enums.CornerShape;
 import net.b34tzepz.betterbuilding.block.enums.SideType;
 import net.minecraft.state.property.EnumProperty;
 
 public class Properties extends net.minecraft.state.property.Properties {
     public static final EnumProperty<SideType> SIDE_TYPE = EnumProperty.of("type", SideType.class);
+    public static final EnumProperty<CornerShape> CORNER_SHAPE = EnumProperty.of("shape", CornerShape.class);
 }
