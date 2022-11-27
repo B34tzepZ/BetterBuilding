df754f8fef56ff21486efcc76eca028aca09e171
TimPoenitzsch <tim.poenitzsch@st.ovgu.de>
2022-11-27 13:53:28 +0100

    add rideable Entity (Tiger) for learning purposes

--- src/main/java/net/b34tzepz/betterbuilding/BetterBuildingClient.java
+++ src/main/java/net/b34tzepz/betterbuilding/BetterBuildingClient.java
@@ -2,6 +2,7 @@ package net.b34tzepz.betterbuilding;
 
 import net.b34tzepz.betterbuilding.entity.ModEntities;
 import net.b34tzepz.betterbuilding.entity.client.RaccoonRenderer;
+import net.b34tzepz.betterbuilding.entity.client.TigerRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
@@ -9,5 +10,6 @@ public class BetterBuildingClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(ModEntities.RACCOON, RaccoonRenderer::new);
+        EntityRendererRegistry.register(ModEntities.TIGER, TigerRenderer::new);
     }
 }
--- src/main/java/net/b34tzepz/betterbuilding/entity/ModEntities.java
+++ src/main/java/net/b34tzepz/betterbuilding/entity/ModEntities.java
@@ -2,6 +2,7 @@ package net.b34tzepz.betterbuilding.entity;
 
 import net.b34tzepz.betterbuilding.BetterBuilding;
 import net.b34tzepz.betterbuilding.entity.custom.RaccoonEntity;
+import net.b34tzepz.betterbuilding.entity.custom.TigerEntity;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
@@ -14,4 +15,9 @@ public class ModEntities {
             Registry.ENTITY_TYPE, new Identifier(BetterBuilding.MOD_ID, "raccoon"),
             FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, RaccoonEntity::new)
                     .dimensions(EntityDimensions.fixed(0.4f, 0.3f)).build());
+
+    public static final EntityType<TigerEntity> TIGER = Registry.register(
+            Registry.ENTITY_TYPE, new Identifier(BetterBuilding.MOD_ID, "tiger"),
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, TigerEntity::new)
+                    .dimensions(EntityDimensions.fixed(1f, 0.75f)).build());
 }
