421485a4461bd4a050ff50c058d86baf4488c45b
Erik Schröder <erikschroeder2002@gmail.com>
2022-12-22 19:30:35 +0100

    added table behaviour, bowl can now stand on tables

--- src/main/java/net/b34tzepz/betterbuilding/block/custom/TableBlock.java
+++ src/main/java/net/b34tzepz/betterbuilding/block/custom/TableBlock.java
@@ -4,24 +4,32 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.DirectionProperty;
-import net.minecraft.state.property.Properties;
-import net.minecraft.util.BlockMirror;
-import net.minecraft.util.BlockRotation;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.function.BooleanBiFunction;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.World;
 
 import java.util.stream.Stream;
 
 public class TableBlock extends HorizontalFacingBlock {
 
-    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+
+    public static final BooleanProperty NORTHBOWLPLACED = BooleanProperty.of("north_bowl_placed");
+    public static final BooleanProperty SOUTHBOWLPLACED = BooleanProperty.of("south_bowl_placed");
+    public static final BooleanProperty EASTBOWLPLACED = BooleanProperty.of("east_bowl_placed");
+    public static final BooleanProperty WESTBOWLPLACED = BooleanProperty.of("west_bowl_placed");
 
     public static VoxelShape SHAPE = Stream.of(
             Block.createCuboidShape(12, 0, 2, 14, 12, 4),
@@ -33,6 +41,11 @@ public class TableBlock extends HorizontalFacingBlock {
 
     public TableBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(((((this.stateManager.getDefaultState()
+                .with(NORTHBOWLPLACED, false))
+                .with(SOUTHBOWLPLACED, false))
+                .with(EASTBOWLPLACED, false))
+                .with(WESTBOWLPLACED, false)));
     }
 
     @Override
@@ -40,24 +53,40 @@ public class TableBlock extends HorizontalFacingBlock {
         return SHAPE;
     }
 
-    @Nullable
-    @Override
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
-    }
-
-    @Override
-    public BlockState rotate(BlockState state, BlockRotation rotation) {
-        return state.with(FACING, rotation.rotate(state.get(FACING)));
-    }
-
-    @Override
-    public BlockState mirror(BlockState state, BlockMirror mirror) {
-        return state.rotate(mirror.getRotation(state.get(FACING)));
-    }
-
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(NORTHBOWLPLACED,SOUTHBOWLPLACED,EASTBOWLPLACED,WESTBOWLPLACED);
     }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        boolean north = state.get(NORTHBOWLPLACED);
+        boolean west = state.get(WESTBOWLPLACED);
+        boolean south = state.get(SOUTHBOWLPLACED);
+        boolean east = state.get(EASTBOWLPLACED);
+
+        if(itemStack.isOf(Items.BOWL) && hand == Hand.MAIN_HAND){
+            Direction dir = player.getHorizontalFacing();
+
+            if(dir == Direction.NORTH && !south){
+                world.setBlockState(pos, state.with(SOUTHBOWLPLACED, true), NOTIFY_ALL);
+            }
+            else if(dir == Direction.WEST && !east){
+                world.setBlockState(pos, state.with(EASTBOWLPLACED, true), NOTIFY_ALL);
+            }
+            else if(dir == Direction.EAST && !west){
+                world.setBlockState(pos, state.with(WESTBOWLPLACED, true), NOTIFY_ALL);
+            }
+            else if(dir == Direction.SOUTH && !north){
+                world.setBlockState(pos, state.with(NORTHBOWLPLACED, true), NOTIFY_ALL);
+            }
+            return ActionResult.SUCCESS;
+        }
+
+        return ActionResult.PASS;
+    }
+
+
+
 }
