e61bd8becfdd0011c54d34e8d67384479a323328
Florian Gräber <florian.graeber@st.ovgu.de>
2023-02-20 17:43:16 +0100

    corner variants

--- src/main/java/net/b34tzepz/betterbuilding/block/ModBlocks.java
+++ src/main/java/net/b34tzepz/betterbuilding/block/ModBlocks.java
@@ -15,7 +15,7 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class ModBlocks{
+public class ModBlocks {
     public static final Block REINFORCED_OAK_PLANKS = registerBlock("reinforced_oak_planks",
             new Block(FabricBlockSettings.of(Material.WOOD).strength(3.5f, 5).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
 
@@ -26,31 +26,52 @@ public class ModBlocks{
             new Block(FabricBlockSettings.of(Material.WOOD).strength(3.5f, 5).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
 
     public static final Block OAK_SIDE = registerBlock("oak_side",
-            new SideBlock(FabricBlockSettings.of(Material.WOOD).strength(2, 3).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
+            new SideBlock(FabricBlockSettings.copyOf(Blocks.OAK_SLAB)), ModItemGroup.Building);
 
     public static final Block SPRUCE_SIDE = registerBlock("spruce_side",
-            new SideBlock(FabricBlockSettings.of(Material.WOOD).strength(2, 3).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
+            new SideBlock(FabricBlockSettings.copyOf(Blocks.SPRUCE_SLAB)), ModItemGroup.Building);
 
     public static final Block BIRCH_SIDE = registerBlock("birch_side",
-            new SideBlock(FabricBlockSettings.of(Material.WOOD).strength(2, 3).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
+            new SideBlock(FabricBlockSettings.copyOf(Blocks.BIRCH_SLAB)), ModItemGroup.Building);
 
     public static final Block JUNGLE_SIDE = registerBlock("jungle_side",
-            new SideBlock(FabricBlockSettings.of(Material.WOOD).strength(2, 3).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
+            new SideBlock(FabricBlockSettings.copyOf(Blocks.JUNGLE_SLAB)), ModItemGroup.Building);
 
     public static final Block ACACIA_SIDE = registerBlock("acacia_side",
-            new SideBlock(FabricBlockSettings.of(Material.WOOD).strength(2, 3).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
+            new SideBlock(FabricBlockSettings.copyOf(Blocks.ACACIA_SLAB)), ModItemGroup.Building);
 
     public static final Block DARK_OAK_SIDE = registerBlock("dark_oak_side",
-            new SideBlock(FabricBlockSettings.of(Material.WOOD).strength(2, 3).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
+            new SideBlock(FabricBlockSettings.copyOf(Blocks.DARK_OAK_SLAB)), ModItemGroup.Building);
 
     public static final Block CRIMSON_SIDE = registerBlock("crimson_side",
-            new SideBlock(FabricBlockSettings.of(Material.WOOD).strength(2, 3).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
+            new SideBlock(FabricBlockSettings.copyOf(Blocks.CRIMSON_SLAB)), ModItemGroup.Building);
 
     public static final Block WARPED_SIDE = registerBlock("warped_side",
-            new SideBlock(FabricBlockSettings.of(Material.WOOD).strength(2, 3).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
+            new SideBlock(FabricBlockSettings.copyOf(Blocks.WARPED_SLAB)), ModItemGroup.Building);
 
     public static final Block OAK_CORNER = registerBlock("oak_corner",
-            new CornerBlock(Blocks.OAK_PLANKS.getDefaultState(), FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
+            new CornerBlock(FabricBlockSettings.copyOf(Blocks.OAK_STAIRS)), ModItemGroup.Building);
+
+    public static final Block SPRUCE_CORNER = registerBlock("spruce_corner",
+            new CornerBlock(FabricBlockSettings.copyOf(Blocks.SPRUCE_STAIRS)), ModItemGroup.Building);
+
+    public static final Block BIRCH_CORNER = registerBlock("birch_corner",
+            new CornerBlock(FabricBlockSettings.copyOf(Blocks.BIRCH_STAIRS)), ModItemGroup.Building);
+
+    public static final Block JUNGLE_CORNER = registerBlock("jungle_corner",
+            new CornerBlock(FabricBlockSettings.copyOf(Blocks.JUNGLE_STAIRS)), ModItemGroup.Building);
+
+    public static final Block ACACIA_CORNER = registerBlock("acacia_corner",
+            new CornerBlock(FabricBlockSettings.copyOf(Blocks.ACACIA_STAIRS)), ModItemGroup.Building);
+
+    public static final Block DARK_OAK_CORNER = registerBlock("dark_oak_corner",
+            new CornerBlock(FabricBlockSettings.copyOf(Blocks.DARK_OAK_STAIRS)), ModItemGroup.Building);
+
+    public static final Block CRIMSON_CORNER = registerBlock("crimson_corner",
+            new CornerBlock(FabricBlockSettings.copyOf(Blocks.CRIMSON_STAIRS)), ModItemGroup.Building);
+
+    public static final Block WARPED_CORNER = registerBlock("warped_corner",
+            new CornerBlock(FabricBlockSettings.copyOf(Blocks.WARPED_STAIRS)), ModItemGroup.Building);
 
     public static final Block OAK_CHAIR = registerBlock("oak_chair",
             new OakChairBlock(FabricBlockSettings.of(Material.WOOD).strength(2, 3).nonOpaque().sounds(BlockSoundGroup.WOOD)), ModItemGroup.Building);
