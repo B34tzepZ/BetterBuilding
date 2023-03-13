c985e22715457b85cc9dd84f1dfbb43a4dc07772
Florian Gräber <florian.graeber@st.ovgu.de>
2023-03-13 14:54:21 +0100

    fuels and flammable registered

--- src/main/java/net/b34tzepz/betterbuilding/util/ModRegistries.java
+++ src/main/java/net/b34tzepz/betterbuilding/util/ModRegistries.java
@@ -1,12 +1,54 @@
 package net.b34tzepz.betterbuilding.util;
 
-import net.b34tzepz.betterbuilding.entity.ModEntities;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.b34tzepz.betterbuilding.BetterBuilding;
+import net.b34tzepz.betterbuilding.block.ModBlocks;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 
 public class ModRegistries {
     public static void registerModStuffs() {
-        registerAttributes();
+        registerFuels();
+        registerFlammables();
     }
-    private static void registerAttributes() {
+
+    private static void registerFuels() {
+        BetterBuilding.LOGGER.info("Registering Fuels for " + BetterBuilding.MOD_ID);
+        FuelRegistry registry = FuelRegistry.INSTANCE;
+
+        registry.add(ModBlocks.OAK_SIDE, 150);
+        registry.add(ModBlocks.SPRUCE_SIDE, 150);
+        registry.add(ModBlocks.BIRCH_SIDE, 150);
+        registry.add(ModBlocks.JUNGLE_SIDE, 150);
+        registry.add(ModBlocks.ACACIA_SIDE, 150);
+        registry.add(ModBlocks.DARK_OAK_SIDE, 150);
+
+        registry.add(ModBlocks.OAK_CORNER, 300);
+        registry.add(ModBlocks.SPRUCE_CORNER, 300);
+        registry.add(ModBlocks.BIRCH_CORNER, 300);
+        registry.add(ModBlocks.JUNGLE_CORNER, 300);
+        registry.add(ModBlocks.ACACIA_CORNER, 300);
+        registry.add(ModBlocks.DARK_OAK_CORNER, 300);
+    }
+
+    private static void registerFlammables() {
+        BetterBuilding.LOGGER.info("Registering Flammables for " + BetterBuilding.MOD_ID);
+        FlammableBlockRegistry registry = FlammableBlockRegistry.getDefaultInstance();
+
+        registry.add(ModBlocks.OAK_SIDE, 20, 5);
+        registry.add(ModBlocks.SPRUCE_SIDE, 20, 5);
+        registry.add(ModBlocks.BIRCH_SIDE, 20, 5);
+        registry.add(ModBlocks.JUNGLE_SIDE, 20, 5);
+        registry.add(ModBlocks.ACACIA_SIDE, 20, 5);
+        registry.add(ModBlocks.DARK_OAK_SIDE, 20, 5);
+
+        registry.add(ModBlocks.OAK_CORNER,20,5);
+        registry.add(ModBlocks.SPRUCE_CORNER,20,5);
+        registry.add(ModBlocks.BIRCH_CORNER,20,5);
+        registry.add(ModBlocks.JUNGLE_CORNER,20,5);
+        registry.add(ModBlocks.ACACIA_CORNER,20,5);
+        registry.add(ModBlocks.DARK_OAK_CORNER,20,5);
+
+        registry.add(ModBlocks.TELEPORTER_BLOCK, 0,0);
+
     }
 }
