b959076a8807e96ae22e0723909a78fcd4f6ddb9
Florian Gräber <florian.graeber@st.ovgu.de>
2023-03-31 22:29:36 +0200

    mod item group for pillars

--- src/main/java/net/b34tzepz/betterbuilding/item/ModItemGroup.java
+++ src/main/java/net/b34tzepz/betterbuilding/item/ModItemGroup.java
@@ -1,6 +1,7 @@
 package net.b34tzepz.betterbuilding.item;
 
 import net.b34tzepz.betterbuilding.BetterBuilding;
+import net.b34tzepz.betterbuilding.block.ModBlocks;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -8,5 +9,8 @@ import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
     public static final ItemGroup Building = FabricItemGroupBuilder.build(
-                    new Identifier(BetterBuilding.MOD_ID, "building"), () -> new ItemStack(ModItems.SCREW));
+            new Identifier(BetterBuilding.MOD_ID, "building"), () -> new ItemStack(ModItems.SCREW));
+
+    public static final ItemGroup Pillars = FabricItemGroupBuilder.build(
+            new Identifier(BetterBuilding.MOD_ID, "pillars"), () -> new ItemStack(ModBlocks.OAK_LOG_PILLAR));
 }
