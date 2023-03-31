78bab094d6b754817ea232c584871d819b8c0487
Florian Gräber <florian.graeber@st.ovgu.de>
2023-03-31 22:22:15 +0200

    chisel registered

--- src/main/java/net/b34tzepz/betterbuilding/item/ModItems.java
+++ src/main/java/net/b34tzepz/betterbuilding/item/ModItems.java
@@ -1,11 +1,10 @@
 package net.b34tzepz.betterbuilding.item;
 
 import net.b34tzepz.betterbuilding.BetterBuilding;
-import net.b34tzepz.betterbuilding.entity.ModEntities;
+import net.b34tzepz.betterbuilding.item.custom.ChiselItem;
 import net.b34tzepz.betterbuilding.item.custom.DowsingRodItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.item.SpawnEggItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -13,11 +12,14 @@ public class ModItems {
     public static final Item SCREW = registerItem("screw",              //Screw is colored because it's the icon of ModItemGroup.Building
             new Item(new FabricItemSettings().group(ModItemGroup.Building)));
 
-    public static final Item Table_Leg = registerItem("table_leg",
+    public static final Item TABLE_LEG = registerItem("table_leg",
             new Item(new FabricItemSettings().group(ModItemGroup.Building)));
 
-    public static final Item Dowsing_Rod = registerItem("dowsing_rod",
+    public static final Item DOWSING_ROD = registerItem("dowsing_rod",
             new DowsingRodItem(new FabricItemSettings().group(ModItemGroup.Building).maxDamage(64)));
+
+    public static final Item CHISEL = registerItem("chisel",
+            new ChiselItem(new FabricItemSettings().group(ModItemGroup.Building).maxDamage(64)));
 
 
     private static Item registerItem(String name, Item item){
