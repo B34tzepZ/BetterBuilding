a85dcdc50a12186ac6dcddfc0c11a1bea584836c
Florian Gräber <florian.graeber@st.ovgu.de>
2023-03-31 22:22:44 +0200

    ModTags class

---
+++ src/main/java/net/b34tzepz/betterbuilding/util/ModTags.java
@@ -0,0 +1,42 @@
+package net.b34tzepz.betterbuilding.util;
+
+
+import net.b34tzepz.betterbuilding.BetterBuilding;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModTags {
+    public static class Blocks{
+        public static final TagKey<Block> DOWSING_ROD_DETECTABLE_BLOCKS = createTag("dowsing_rod_detectable_blocks");
+        public static final TagKey<Block> SIDES = createTag("sides");
+        public static final TagKey<Block> CORNERS = createTag("corners");
+        public static final TagKey<Block> WOODEN_SIDES = createTag("wooden_sides");
+        public static final TagKey<Block> WOODEN_CORNERS = createTag("wooden_corners");
+
+        private static TagKey<Block> createTag(String name){
+            return TagKey.of(Registry.BLOCK_KEY, new Identifier(BetterBuilding.MOD_ID, name));
+        }
+
+        private  static TagKey<Block> createCommonTag(String name){
+            return TagKey.of(Registry.BLOCK_KEY, new Identifier("c", name));
+        }
+    }
+
+    public static class Items{
+        public static final TagKey<Item> SIDES = createTag("sides");
+        public static final TagKey<Item> CORNERS = createTag("corners");
+        public static final TagKey<Item> WOODEN_SIDES = createTag("wooden_sides");
+        public static final TagKey<Item> WOODEN_CORNERS = createTag("wooden_corners");
+
+        private static TagKey<Item> createTag(String name){
+            return TagKey.of(Registry.ITEM_KEY, new Identifier(BetterBuilding.MOD_ID, name));
+        }
+
+        private  static TagKey<Item> createCommonTag(String name){
+            return TagKey.of(Registry.ITEM_KEY, new Identifier("c", name));
+        }
+    }
+}
