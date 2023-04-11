d62af16cde0fbf85f67f75131ef50994ef6e3698
TimPoenitzsch <tim.poenitzsch@st.ovgu.de>
2023-04-11 18:22:18 +0200

    make chairs flammable and usable as fuel

--- src/main/java/net/b34tzepz/betterbuilding/util/ModRegistries.java
+++ src/main/java/net/b34tzepz/betterbuilding/util/ModRegistries.java
@@ -28,6 +28,13 @@ public class ModRegistries {
         registry.add(ModBlocks.JUNGLE_CORNER, 300);
         registry.add(ModBlocks.ACACIA_CORNER, 300);
         registry.add(ModBlocks.DARK_OAK_CORNER, 300);
+
+        registry.add(ModBlocks.OAK_CHAIR, 300);
+        registry.add(ModBlocks.SPRUCE_CHAIR, 300);
+        registry.add(ModBlocks.BIRCH_CHAIR, 300);
+        registry.add(ModBlocks.JUNGLE_CHAIR, 300);
+        registry.add(ModBlocks.ACACIA_CHAIR, 300);
+        registry.add(ModBlocks.DARK_OAK_CHAIR, 300);
     }
 
     private static void registerFlammables() {
@@ -47,6 +54,13 @@ public class ModRegistries {
         registry.add(ModBlocks.JUNGLE_CORNER,20,5);
         registry.add(ModBlocks.ACACIA_CORNER,20,5);
         registry.add(ModBlocks.DARK_OAK_CORNER,20,5);
+
+        registry.add(ModBlocks.OAK_CHAIR,20,5);
+        registry.add(ModBlocks.SPRUCE_CHAIR,20,5);
+        registry.add(ModBlocks.BIRCH_CHAIR,20,5);
+        registry.add(ModBlocks.JUNGLE_CHAIR,20,5);
+        registry.add(ModBlocks.ACACIA_CHAIR,20,5);
+        registry.add(ModBlocks.DARK_OAK_CHAIR,20,5);
 
         registry.add(ModBlocks.TELEPORTER_BLOCK, 0,0);
 
