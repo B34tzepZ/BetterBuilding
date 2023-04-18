9d03ad290728789139084f8c3dfcd8a1a2e0e867
Florian Gräber <florian.graeber@st.ovgu.de>
2023-04-18 18:54:54 +0200

    copy loot table method

--- src/main/java/net/b34tzepz/betterbuilding/block/JsonGenerator.java
+++ src/main/java/net/b34tzepz/betterbuilding/block/JsonGenerator.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class JsonGenerator {
     public static void main(String[] args) throws Exception {
-        copyLootTable(findNewBlock());
+        //copyLootTable(findNewBlock());
     }
 
     private static void generateJsons(String oldMaterial, String newMaterial, String type) throws Exception {
