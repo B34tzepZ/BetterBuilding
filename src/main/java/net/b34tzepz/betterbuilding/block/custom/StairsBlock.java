4b09a9434f9ea70a0802bdea2a676c78ec96d9e3
B34tzepZ <florian.graeber@gmx.de>
2024-01-27 01:25:04 +0100

    StairsBlock to access constructor

---
+++ src/main/java/net/b34tzepz/betterbuilding/block/custom/StairsBlock.java
@@ -0,0 +1,9 @@
+package net.b34tzepz.betterbuilding.block.custom;
+
+import net.minecraft.block.BlockState;
+
+public class StairsBlock extends net.minecraft.block.StairsBlock {
+    public StairsBlock(BlockState baseBlockState, Settings settings) {
+        super(baseBlockState, settings);
+    }
+}
