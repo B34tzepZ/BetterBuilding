3f51dd156a53da592a413778f18b8a0be1cd1fd1
B34tzepZ <florian.graeber@gmx.de>
2024-02-23 22:57:50 +0100

    PillarMudBlock added

---
+++ src/main/java/net/b34tzepz/betterbuilding/block/custom/PillarMudBlock.java
@@ -0,0 +1,28 @@
+package net.b34tzepz.betterbuilding.block.custom;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+
+public class PillarMudBlock extends PillarBlock {
+    protected static final VoxelShape COLLISION_SHAPE = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 14.0, 16.0);
+
+    public PillarMudBlock(AbstractBlock.Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return COLLISION_SHAPE;
+    }
+
+    @Override
+    public float getAmbientOcclusionLightLevel(BlockState state, BlockView world, BlockPos pos) {
+        return 0.2f;
+    }
+}
+
