cdaaf4461378e7187e8691937be13cac402515b2
B34tzepZ <florian.graeber@gmx.de>
2024-03-14 01:54:27 +0100

    glass pillar translucent

--- src/main/java/net/b34tzepz/betterbuilding/BetterBuildingClient.java
+++ src/main/java/net/b34tzepz/betterbuilding/BetterBuildingClient.java
@@ -14,6 +14,24 @@ public class BetterBuildingClient implements ClientModInitializer {
     public void onInitializeClient() {
         ScreenRegistry.register(ModScreenHandlers.TELEPORTER_BLOCK_SCREEN_HANDLER, TeleporterBlockScreen::new);
         ScreenRegistry.register(ModScreenHandlers.FABRICATOR_SCREEN_HANDLER, FabricatorScreen::new);
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.TINTED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.WHITE_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.ORANGE_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.MAGENTA_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.LIGHT_BLUE_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.YELLOW_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.LIME_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.PINK_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.GRAY_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.LIGHT_GRAY_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.CYAN_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.PURPLE_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.BLUE_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.BROWN_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.GREEN_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.RED_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.Pillars.BLACK_STAINED_GLASS_PILLAR,RenderLayer.getTranslucent());
         //BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks./*Add Block here*/, RenderLayer.getCutout()); //for blocks with transparency like doors, many other options for rendering instead of getCutout()-
     }
 }
